1f3ece183feec7e8d813dc73034af2c9c4fb8b3c
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-02-14 16:57:15 -0500

    created todo and tictactoe template files

---
+++ src/components/Tictactoe.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function Tictactoe() {
+  return (
+    <div className="container">
+      <h2>Tic-Tac-Toe</h2>
+      <div></div>
+    </div>
+  );
+}
---
+++ src/components/Todos.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function ToDo() {
+  return (
+    <div className="container">
+      <h2>To Do List</h2>
+      <div></div>
+    </div>
+  );
+}
